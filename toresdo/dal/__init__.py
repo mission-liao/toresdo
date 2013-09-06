bfa5ec91a92344a83329ede6edf03748b661d5b9
mission.liao <missionaryliao@gmail.com>
2013-09-06 16:32:17 +0800

    remote __init__.py
    later rename base.py to __init__.py

--- toresdo/dal/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import
-
-from toresdo.dal.base import AdapterBase
-from toresdo.dal.base import field
-from toresdo.dal.base import Cond
-from toresdo.dal.base import ConnPool
